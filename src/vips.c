2b3aeda020b91abd43f48add2c10f34fc425e475
Masatoshi Teruya <mah0x211@gmail.com>
2016-11-18 19:22:39 +0900

    Add newFromFile function

--- src/vips.c
+++ src/vips.c
@@ -35,9 +35,137 @@
 #include "vips/vips.h"
 
 
+#define MODULE_MT           "vips"
+#define MODULE_IMAGE_MT     "vips.image"
+
+#define DEFAULT_QUALITY     85
+
+typedef struct {
+    VipsImage *img;
+    uint8_t q;
+} lvips_t;
+
+
+static int tostring_lua( lua_State *L )
+{
+    lua_pushfstring( L, MODULE_MT ": %p", lua_touserdata( L, 1 ) );
+    return 1;
+}
+
+
+static int gc_lua( lua_State *L )
+{
+    lvips_t *v = luaL_checkudata( L, 1, MODULE_IMAGE_MT );
+
+    if( v->img ){
+        VIPS_UNREF( v->img );
+    }
+
+    return 0;
+}
+
+
+static int newfromfile_lua( lua_State *L )
+{
+    const char *pathname = lauxh_checkstring( L, 1 );
+    lvips_t *v = lua_newuserdata( L, sizeof( lvips_t ) );
+
+    // mem error
+    if( !v ){
+        lua_pushnil( L );
+        lua_pushstring( L, strerror( errno ) );
+        return 2;
+    }
+
+    v->img = vips_image_new_from_file( pathname, "access",
+                                       VIPS_ACCESS_SEQUENTIAL, NULL );
+    if( v->img ){
+        lauxh_setmetatable( L, MODULE_IMAGE_MT );
+        v->q = DEFAULT_QUALITY;
+        return 1;
+    }
+
+    // got error
+    lua_pushnil( L );
+    lua_pushstring( L, vips_error_buffer() );
+    vips_error_clear();
+
+    return 2;
+}
+
+
+static int gc_module( lua_State *L )
+{
+    vips_shutdown();
+    return 0;
+}
+
+
+static int init_module( lua_State *L )
+{
+    // init vips global
+    if( VIPS_INIT( MODULE_MT ) ){
+        return luaL_error( L, "failed to VIPS_INIT()" );
+    }
+
+    // create metatable
+    luaL_newmetatable( L, MODULE_MT );
+    lauxh_pushfn2tbl( L, "__gc", gc_module );
+    // remove metatable from stack
+    lua_pop( L, 1 );
+
+    // create module data
+    if( !lua_newuserdata( L, sizeof(0) ) ){
+        return luaL_error( L, "failed to create lua_newuserdata" );
+    }
+    lauxh_setmetatable( L, MODULE_MT );
+    lauxh_ref( L );
+
+    return 0;
+}
+
+
 LUALIB_API int luaopen_vips( lua_State *L )
 {
-    return 0;
+    struct luaL_Reg mmethods[] = {
+        { "__gc", gc_lua },
+        { "__tostring", tostring_lua },
+        { NULL, NULL }
+    };
+    struct luaL_Reg methods[] = {
+        { NULL, NULL }
+    };
+    struct luaL_Reg *ptr = mmethods;
+
+    // init vips global
+    if( init_module( L ) == -1 ){
+        return 0;
+    }
+
+    // create metatable
+    luaL_newmetatable( L, MODULE_IMAGE_MT );
+    // metamethods
+    while( ptr->name ){
+        lauxh_pushfn2tbl( L, ptr->name, ptr->func );
+        ptr++;
+    }
+    // methods
+    ptr = methods;
+    lua_pushstring( L, "__index" );
+    lua_newtable( L );
+    while( ptr->name ){
+        lauxh_pushfn2tbl( L, ptr->name, ptr->func );
+        ptr++;
+    }
+    lua_rawset( L, -3 );
+    // remove metatable from stack
+    lua_pop( L, 1 );
+
+    // register allocator
+    lua_createtable( L, 0, 1 );
+    lauxh_pushfn2tbl( L, "newFromFile", newfromfile_lua );
+
+    return 1;
 }
 
 
